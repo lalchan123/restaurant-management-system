cfc6125a76c81dd765956cf21d0285728cfbaa6b
lalchan123 <lalchanbadsa12345@gmail.com>
2025-09-18 10:56:37 -0600

    18-09-2025 by lalcha

--- src/ApiCallMethod/Constants.js
+++ src/ApiCallMethod/Constants.js
@@ -3,4 +3,4 @@
 // export const FrontEndBaseURL = 'http://localhost:3000';
 export const BaseURL = 'https://tasteofindiamckinney.net';
 // export const BaseURL1 = 'http://38.107.232.191:8000';
-export const FrontEndBaseURL = 'http://localhost:3000';
+export const FrontEndBaseURL = 'https://www.tasteofindiamckinney.net';
